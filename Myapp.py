bb0301f535507899450a5c60824c6339f4c24e3f
vvvvv19 <1009957030@qq.com>
2024-09-27 03:18:31 +0800

    update Myapp 更新Myapp，拥有初级的输入参数功能

--- Myapp.py
+++ Myapp.py
@@ -1,12 +1,10 @@
 import argparse
+import random
 
-def generate_questions(num):
-    for i in range(num):
-        print(f"Question {i + 1}")
+def main():
+    parser = argparse.ArgumentParser(description='生成数学题目。')
+    parser.add_argument('-n', type=int, required=True, help='生成题目的数量')
+    parser.add_argument('-r', type=int, required=True, help='题目中数值的范围')
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', type=int, required=True, help='Number of questions to generate')
-    args = parser.parse_args()
-
-    generate_questions(args.n)
+if __name__ == '__main__':
+    main()
